673d9597b35034243a007ccbbb8d6d76e4d0d0ea
Marian Mavrichi <marian.mavrichi@gmail.com>
2020-12-02 02:43:25 +0200

    build maybe works now ?

---
+++ build/static/js/xlsx.56bbb027.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[2],{240:function(p,n){},330:function(p,n){},331:function(p,n){}}]);
+//# sourceMappingURL=xlsx.56bbb027.chunk.js.map
